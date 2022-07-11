ef82c2040abfb9f504807588cd0883634ab40c3d
Daniel Peñaloza <danielpenaloza@Daniels-MacBook-Pro.local>
2022-07-11 10:43:18 -0500

    Implement getPhotos on PhotoViewModel to fetch photos

--- app/src/main/java/com/example/photosearcher/PhotoViewModel.kt
+++ app/src/main/java/com/example/photosearcher/PhotoViewModel.kt
@@ -3,24 +3,35 @@ package com.example.photosearcher
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.photosearcher.data.Photo
+import com.example.photosearcher.data.Result
 import com.example.photosearcher.data.source.PhotoRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class PhotoViewModel @Inject constructor(
     private val photoRepository: PhotoRepository
-): ViewModel() {
+) : ViewModel() {
 
     private val _photos = MutableLiveData<List<Photo>>()
-    val photos : LiveData<List<Photo>> = _photos
+    val photos: LiveData<List<Photo>> = _photos
 
     private val _dataLoading = MutableLiveData<Boolean>()
     val dataLoading: LiveData<Boolean> = _dataLoading
 
-    fun getPhotos(){
-
+    fun getPhotos() {
+        _dataLoading.value = true
+        viewModelScope.launch(Dispatchers.IO) {
+            val result = photoRepository.getPhotos()
+            if (result is Result.Success) {
+                _photos.value = result.data
+            }
+            _dataLoading.value = false
+        }
     }
 
 }
